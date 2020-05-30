034c243febc2861a4491021ca6d295bbd89fa418
Joshua Poddoku <201751033@iiitvadodara.ac.in>
2020-05-31 00:45:22 +0530

    day 2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 var map;
 var markers = [];
-function initMap() {    
+var infoWindow;
+function initMap() {
+    
+    
+    
     var losAngeles = {
         lat: 34.063380,
         lng: -118.358080
@@ -8,8 +12,91 @@ function initMap() {
 
 map = new google.maps.Map(document.getElementById('map'), {
     center: losAngeles,
-    zoom: 8
-});
+    zoom: 8,
+    styles: [
+        {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+        {
+          featureType: 'administrative.locality',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'geometry',
+          stylers: [{color: '#263c3f'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#6b9a76'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry',
+          stylers: [{color: '#38414e'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#212a37'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#9ca5b3'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry',
+          stylers: [{color: '#746855'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#1f2835'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#f3d19c'}]
+        },
+        {
+          featureType: 'transit',
+          elementType: 'geometry',
+          stylers: [{color: '#2f3948'}]
+        },
+        {
+          featureType: 'transit.station',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'geometry',
+          stylers: [{color: '#17263c'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#515c6d'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.stroke',
+          stylers: [{color: '#17263c'}]
+        }
+      ]
+}
+
+
+);
 
 var marker = new google.maps.Marker({
     position: losAngeles,
@@ -17,7 +104,7 @@ var marker = new google.maps.Marker({
     title: 'Hello World!'
     });
 
-
+    infoWindow = new google.maps.InfoWindow();
     displayStores();
     showStoresMarkers();
 
@@ -59,15 +146,16 @@ function showStoresMarkers(){
     var bounds = new google.maps.LatLngBounds();
     stores.forEach(function (store, index){
         var latlng = new google.maps.LatLng(
-            stores.coordinates.latitude,
-            stores.coordinates.longitude); 
+            store.coordinates.latitude,
+            store.coordinates.longitude); 
+        console.log(latlng);    
         var name = store.name;
         var address = store.addressLines[0];
+        bounds.extend(latlng);
         createMarker(latlng, name, address);
         
 })
-
-bounds.extend(latlng);
+map.fitBounds(bounds);
 }
 
 function createMarker(latlng, name, address) {
@@ -76,9 +164,9 @@ function createMarker(latlng, name, address) {
       map: map,
       position: latlng
     });
-    // google.maps.event.addListener(marker, 'click', function() {
-    //   infoWindow.setContent(html);
-    //   infoWindow.open(map, marker);
-    // });
+    google.maps.event.addListener(marker, 'click', function() {
+      infoWindow.setContent(html);
+      infoWindow.open(map, marker);
+    });
     markers.push(marker);
   }
